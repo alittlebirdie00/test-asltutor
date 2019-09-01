69d25209086c66276c47ef139ef072df9cee26b3
Rolando Murillo <murillorafael7@gmail.com>
2019-09-01 10:20:29 -0400

    first test commit

---
+++ test-asltutor/test-asltutor WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  test-asltutor WatchKit Extension
+//
+//  Created by Rolando Murillo III on 9/1/19.
+//  Copyright © 2019 Rolando Murillo III. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        // hhi
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ test-asltutor/test-asltutor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-asltutor
+//
+//  Created by Rolando Murillo III on 9/1/19.
+//  Copyright © 2019 Rolando Murillo III. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
